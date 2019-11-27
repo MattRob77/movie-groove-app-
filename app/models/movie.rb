bd4b8a332fef760918a83b1ef92babcc34195b79
github username <github email address>
2019-11-27 17:20:32 -0500

    added validations, created spec.md checklist, refractured controllers

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
   belongs_to :genre
   belongs_to :user
+  has_many :reviews
+  has_many :users, through: :reviews 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  has_many :reviews
+  has_many :reviewed_movies, through: :reviews, source: :movie
+
+  has_many :movies #That the user has created
+
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true 
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
