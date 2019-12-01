5d7d50f5a6d0dcbad0aea4e9d3486ece789f83ea
github username <github email address>
2019-12-01 10:34:57 -0500

    refractured controllers, updated relationships, added index to movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,27 @@
 class MoviesController < ApplicationController
 
+  def index
+    @movies = Movies.all
+  end
+
   def new
-    @movie = Movie.new 
+    @movie = Movie.new
+    @movie.build_genre
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      @movie.build_genre
+      render :new
+    end
+end
+
+private
+
+  def movie_params
+    params.require(:movie).permit(:name, :description, :user_id)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ApplicationRecord
   belongs_to :genre
-  belongs_to :user
   has_many :reviews
-  has_many :users, through: :reviews 
+  has_many :users, through: :reviews
 end
