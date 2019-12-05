df87210eae4817aa44c374f6cef4e2acc291b2be
github username <github email address>
2019-12-05 11:19:54 -0500

    added new migration for omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ApplicationRecord
   has_many :movies #That the user has created
 
   validates :username, uniqueness: true, presence: true
-  validates :email, uniqueness: true, presence: true 
+  validates :email, uniqueness: true, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   get 'movies/grouped_ratings', to: 'movies#ratings'
   resources :reviews
   resources :movies do
-    resources :reviews, only: [:new, :index]
+  resources :reviews, only: [:new, :index]
   end
   resources :genres
   devise_for :users, :controllers => {registrations: 'registrations' }
+
 
   root to: 'application#welcome'
 end
