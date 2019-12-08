d98910f354a25a88baa219f8749181f321132ed1
github username <github email address>
2019-12-08 14:12:05 -0500

    Added CSS to stylesheets

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,8 +17,7 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review =
-    current_user.reviews.build(review_params)
+    @review = current_user.reviews.build(review_params)
     if @review.save
         redirect_to review_path(@review)
     else
@@ -28,6 +27,16 @@ class ReviewsController < ApplicationController
 
   def show
     @review = Review.find_by_id(params[:id])
+  end
+
+  def edit
+      @review = Review.find_by_id(params[:id])
+  end 
+
+  def update
+    @review = Review.find_by_id(params[:id])
+    @review.update(review_params)
+    redirect_to review_path(@review)
   end
 
   private
